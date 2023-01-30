ec1c2a30df4432619d618e864e4a418abdd123cc
Rafael Sá <sanunesrafael@gmail.com>
2023-01-30 11:53:27 -0300

    creating a first component

---
+++ react-todo-demo/react-todo/src/components/TodoRowItem.js
@@ -0,0 +1,12 @@
+function TodoRowItem() {
+  return(
+    <tr>
+      <th scope='row'>1</th>
+      <td>Feed dog</td>
+      <td>Eric</td>
+    </tr>
+  )
+}
+
+//este comando permite que usemos esse componente dentro da nossa aplicação
+export default TodoRowItem
